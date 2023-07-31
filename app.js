ecfcc26ef31fe787a08921b1b5b42ad803e1199f
Joshua Coleman <50680485+Focuscode77@users.noreply.github.com>
2023-07-31 18:54:54 -0400

    Javascript basics
    Basic function practice refesher.

---
+++ app.js
@@ -0,0 +1,35 @@
+//This is a note in a javascript file
+
+//These are examples of functions within javascript
+
+const myFunction = () => {
+    console.log("I am in the function");
+
+}
+// Invoking a function aswell as calling the function
+myFunction();
+
+const sayHello = (message) => {
+
+    console.log(message);
+
+}
+
+sayHello("Hello i am passed in the function");
+
+
+//passing parameters
+const newDrinks = (glasscup) => {
+    console.log(glasscup);
+
+}
+
+newDrinks("pepsi");
+
+const newiceCream = (falvors) => {
+
+    console.log(falvors);
+
+}
+
+newiceCream("Peachicecream");
